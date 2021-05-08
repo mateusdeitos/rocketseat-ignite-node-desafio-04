acc8c1023b9268376ff3a26e9e1fffb0715f809f
mateusdeitos <mate.deitos@hotmail.com>
2021-05-08 10:48:41 -0300

    Desafio 04 node

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,23 +19,30 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = { ...new User(), name, email };
+    this.users.push(user);
+    return user;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    return this.users.find((u) => u.id === id);
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    return this.users.find((u) => u.email === email);
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    const index = this.users.findIndex((u) => u.id === receivedUser.id);
+    if (index >= 0) {
+      this.users[index] = { ...receivedUser, admin: true };
+      return this.users[index];
+    }
+    return receivedUser;
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,11 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const emailExists = this.usersRepository.findByEmail(email);
+    if (emailExists) {
+      throw new Error("Email already taken");
+    }
+    return this.usersRepository.create({ email, name });
   }
 }
 
